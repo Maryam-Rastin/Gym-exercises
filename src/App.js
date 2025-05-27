adffc96ce2460f852faa701381c7904e964136fa
rastindesign <maryam.s.rastin@gmail.com>
2025-05-27 10:55:55 +0330

    fixing the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./App.css";
 import Navbar from "./components/Navbar";
