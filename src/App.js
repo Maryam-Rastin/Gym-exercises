509a0d408de05fcbc89cc215f7af68e1a9b10c53
Maryam-Rastin <248294243+Maryam-Rastin@users.noreply.github.com>
2026-07-07 19:24:08 +0330

    fixing a router error pt2

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
-      <Navbar />
-      <Router basename="/Gym-exercises/">
+    <Router basename="/Gym-exercises/">
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
-      </Router>
-      <Footer />
-    </Box>
+        <Footer />
+      </Box>
+    </Router>
   );
 };
 
